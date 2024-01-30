4d0bb0496812b683ecedc4830c80907754708c56
Abhishek Shandilya <97976470+abhishandilya@users.noreply.github.com>
2024-01-30 11:22:16 -0500

    add few resources with default props

--- sample-app/lib/control-stack.ts
+++ sample-app/lib/control-stack.ts
@@ -1,8 +1,62 @@
 import * as cdk from "aws-cdk-lib";
+import { RestApi } from "aws-cdk-lib/aws-apigateway";
+import { CloudFrontWebDistribution } from "aws-cdk-lib/aws-cloudfront";
+import { AttributeType, Table } from "aws-cdk-lib/aws-dynamodb";
+import { Vpc } from "aws-cdk-lib/aws-ec2";
+import { HttpMethod } from "aws-cdk-lib/aws-events";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Bucket } from "aws-cdk-lib/aws-s3";
+import { Topic } from "aws-cdk-lib/aws-sns";
+import { Queue } from "aws-cdk-lib/aws-sqs";
+import { CfnWebACL } from "aws-cdk-lib/aws-waf";
 import { Construct } from "constructs";
 
 export class ControlStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    new NodejsFunction(this, "nodejs");
+
+    const bucket = new Bucket(this, "bucket");
+
+    new CloudFrontWebDistribution(this, "cloudfront", {
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: bucket,
+          },
+          behaviors: [
+            {
+              isDefaultBehavior: true,
+            },
+          ],
+        },
+      ],
+    });
+
+    const restApi = new RestApi(this, "restApi");
+
+    restApi.root.addMethod(HttpMethod.GET);
+
+    new Table(this, "table", {
+      partitionKey: {
+        name: "id",
+        type: AttributeType.STRING,
+      },
+    });
+
+    new Topic(this, "SnsTopic");
+
+    new Vpc(this, "vpc");
+
+    new Queue(this, "queue");
+
+    new CfnWebACL(this, "WebACL", {
+      defaultAction: {
+        type: "ALLOW",
+      },
+      metricName: "WebACL",
+      name: "WebACL",
+    });
   }
 }
