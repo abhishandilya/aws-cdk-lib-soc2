f49a1de9484cd218b35ea70558c9cb6c088254a0
Abhishek Shandilya <97976470+abhishandilya@users.noreply.github.com>
2024-01-30 20:48:31 -0500

    cdk env vars

--- sample-app/bin/sample-app.ts
+++ sample-app/bin/sample-app.ts
@@ -2,4 +2,9 @@ import * as cdk from "aws-cdk-lib";
 import { ControlStack } from "../lib/control-stack";
 
 const app = new cdk.App();
-new ControlStack(app, "ControlStack", {});
+new ControlStack(app, "ControlStack", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
